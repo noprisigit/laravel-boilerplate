8c552234af94400e7ecd030eb19d86ac88f51a33
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2025-01-07 08:32:39 +0700

    fixing role edit permission

--- app/Livewire/Backend/Role/RoleForm.php
+++ app/Livewire/Backend/Role/RoleForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Backend\Role;
 
+use App\Models\Permission;
 use App\Models\Role;
 use Livewire\Component;
 
@@ -12,6 +13,9 @@ class RoleForm extends Component
     public $type;
     public $form = [];
 
+    public $permissions = [];
+    public $selectedPermissions = [];
+
     public function mount($role = null, $type = '', $action = '')
     {
         $this->action = $action ?? 'store';
@@ -20,7 +24,15 @@ class RoleForm extends Component
         if ($role) {
             $this->role = $role;
             $this->initForm($role);
+            $this->selectedPermissions = $role?->permissions->pluck('id')->toArray() ?? [];
         }
+
+        $this->permissions = Permission::query()
+            ->select('group_name', 'id', 'name')
+            ->orderBy('group_name')
+            ->get()
+            ->groupBy('group_name')
+            ->all();
     }
 
     public function initForm($role = null)
@@ -37,7 +49,8 @@ class RoleForm extends Component
             'form.name' => __('Nama Role')
         ]);
 
-        Role::query()->create($this->form);
+        $role = Role::query()->create($this->form);
+        $role->permissions()->sync($this->selectedPermissions);
 
         session()->flash('success', __('Role baru berhasil ditambahkan'));
 
@@ -53,10 +66,60 @@ class RoleForm extends Component
         ]);
 
         $this->role->update($this->form);
+        $this->role->permissions()->sync($this->selectedPermissions);
 
         session()->flash('success', __('Role berhasil diperbarui'));
 
         $this->redirect(route('roles.index'));
+    }
+
+    public function setSelectedPermission($permission, $group = null)
+    {
+        if ($permission === 'all-permissions') {
+            if ($this->areAllPermissionsSelected()) {
+                // Uncheck all permissions
+                $this->selectedPermissions = [];
+            } else {
+                // Select all permissions
+                $this->selectedPermissions = collect($this->permissions)
+                    ->flatMap(fn($groupPermissions) => $groupPermissions->pluck('id'))
+                    ->toArray();
+            }
+        } elseif ($group !== null && $permission === 'group') {
+            // Toggle all permissions within a specific group
+            $groupPermissions = $this->permissions[$group]->pluck('id')->toArray();
+            if ($this->areGroupPermissionsSelected($groupPermissions)) {
+                // Uncheck all permissions in the group
+                $this->selectedPermissions = array_filter($this->selectedPermissions, fn($id) => !in_array($id, $groupPermissions));
+            } else {
+                // Select all permissions in the group
+                $this->selectedPermissions = array_unique(array_merge($this->selectedPermissions, $groupPermissions));
+            }
+        } else {
+            // Toggle an individual permission
+            if (in_array($permission, $this->selectedPermissions)) {
+                // Remove the permission if already selected
+                $this->selectedPermissions = array_filter($this->selectedPermissions, fn($item) => $item !== $permission);
+            } else {
+                // Add the permission if not already selected
+                $this->selectedPermissions[] = $permission;
+            }
+        }
+
+        // Debugging the selected permissions
+    }
+
+    private function areAllPermissionsSelected(): bool
+    {
+        $allPermissions = collect($this->permissions)
+            ->flatMap(fn($groupPermissions) => $groupPermissions->pluck('id'))
+            ->toArray();
+        return count(array_diff($allPermissions, $this->selectedPermissions)) === 0;
+    }
+
+    private function areGroupPermissionsSelected(array $groupPermissions): bool
+    {
+        return count(array_diff($groupPermissions, $this->selectedPermissions)) === 0;
     }
 
     public function render()
--- resources/views/livewire/backend/role/role-form.blade.php
+++ resources/views/livewire/backend/role/role-form.blade.php
@@ -10,6 +10,51 @@
                 @enderror
             </div>
         </div>
+        <div class="row mb-3">
+            <label for="form.permissions" class="col-sm-2 col-form-label">{{ __('Permissions') }}</label>
+            <div class="col-sm-10">
+                <div>
+                    <div class="custom-toggle-switch d-flex align-items-center mb-4">
+                        <input id="check-all-global" type="checkbox" wire:change="setSelectedPermission('all-permissions')">
+                        <label for="check-all-global" class="label-info"></label><span
+                            class="ms-3">{{ __('Centang Semua Permission') }}</span>
+                    </div>
+
+                    <hr>
+                </div>
+                <div class="row" wire:ignore>
+                    @foreach ($permissions as $groupName => $groupPermissions)
+                        <div class="col-4">
+                            <p>{{ __($groupName) }}</p>
+                            <div class="custom-toggle-switch d-flex align-items-center mb-4">
+                                <input type="checkbox" class="check-all-permissions" id="check-all-{{ $groupName }}"
+                                    data-group="{{ $groupName }}" wire:change="setSelectedPermission('group', '{{ $groupName }}')">
+                                <label for="check-all-{{ $groupName }}" class="label-info"></label><span
+                                    class="ms-3">
+                                    {{ __('Centang Semua ' . $groupName) }}
+                                </span>
+                            </div>
+
+                            @foreach ($groupPermissions as $permission)
+                                <div class="custom-toggle-switch d-flex align-items-center mb-4">
+                                    <input type="checkbox" id="check-{{ $permission->name }}" class="permission"
+                                        value="{{ $permission->id }}" data-group="{{ $groupName }}" wire:model="selectedPermissions"
+                                        @if (in_array($permission->id, $selectedPermissions)) checked @endif
+                                        wire:change="setSelectedPermission('{{ $permission->id }}')">
+                                    <label for="check-{{ $permission->name }}" class="label-info"></label><span
+                                        class="ms-3">
+                                        {{ __($permission->name) }}
+                                    </span>
+                                </div>
+                            @endforeach
+                        </div>
+                    @endforeach
+                </div>
+                @error('form.permissions')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
         <div class="row">
             <div class="col-sm-10 offset-sm-2">
                 <button type="submit" class="btn btn-primary btn-wave" wire:target="{{ $action ?? '' }}"
@@ -32,3 +77,39 @@
         </div>
     </form>
 </div>
+
+@script
+    <script>
+        $(document).ready(function() {
+            $(document).on('change', '#check-all-global', function() {
+                let isChecked = $(this).is(':checked');
+
+                $('.check-all-permissions, .permission').prop('checked', isChecked).trigger('change');
+            });
+
+            $(document).on('change', '.check-all-permissions', function() {
+                const group = $(this).data('group');
+                const isChecked = $(this).is(':checked');
+
+                $(`.permission[data-group="${group}"]`).prop('checked', isChecked).trigger('change');
+
+                updateGlobalCheckAllState();
+            });
+
+            $(document).on('change', '.permission', function() {
+                const group = $(this).data('group');
+                const allChecked = $(`.permission[data-group="${group}"]`).length ===
+                    $(`.permission[data-group="${group}"]:checked`).length;
+
+                $(`.check-all-permissions[data-group="${group}"]`).prop('checked', allChecked);
+
+                updateGlobalCheckAllState();
+            });
+        });
+
+        function updateGlobalCheckAllState() {
+            let allChecked = $('.permission').length === $('.permission:checked').length;
+            $('#check-all-global').prop('checked', allChecked);
+        }
+    </script>
+@endscript
